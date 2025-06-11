cc5c1d94f64c1c60574d70254a7e80aaaa1eb6ec
charubala-b <123437626+charubala-b@users.noreply.github.com>
2025-06-11 12:12:34 +0530

    Add files via upload

---
+++ day2/methods.rb
@@ -0,0 +1,13 @@
+#chaining methods
+puts ['hello!',"how","are","you?"].join(" ")
+
+#bang methods
+str="Rent Smarter"
+str.upcase
+puts str
+str.upcase!
+puts str
+
+#predicate methods
+puts 10.even?
+puts 10.between?(2,10)
