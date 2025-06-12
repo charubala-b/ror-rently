29f6633e3be6580823bac6e5870e883ab0e82b56
charubala-b <123437626+charubala-b@users.noreply.github.com>
2025-06-12 10:59:09 +0530

    Update abstraction.rb

--- day3/abstraction.rb
+++ day3/abstraction.rb
@@ -1,1 +1,13 @@
+class Calculator
+        def publicmethod
+                puts "public method"
+                privatemethod
+        end
+        private
+        def privatemethod
+                puts "private method"
+        end
+end
+obj=Calculator.new
+obj.publicmethod
 
